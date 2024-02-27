35e6ad9c4c1d4f684069ebac25ca80b0763dcbc8
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-02-27 19:07:25 +0530

    upd -j

--- src/Common/headers/index.jsx
+++ src/Common/headers/index.jsx
@@ -17,11 +17,11 @@ export default function NewNav() {
   const handleToggle = () => {
     setToggle(!toggle);
   };
-  if (toggle) {
-    document.body.classList.add("mobile_menu_open");
-  } else {
-    document.body.classList.remove("mobile_menu_open");
-  }
+  // if (toggle) {
+  //   document.body.classList.add("mobile_menu_open");
+  // } else {
+  //   document.body.classList.remove("mobile_menu_open");
+  // }
   return (
     <div className="headerMain">
       <div
