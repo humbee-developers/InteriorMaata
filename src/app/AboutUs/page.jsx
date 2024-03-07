b386439afb1ad6df99ef12a6a91f39271dfb2174
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-03-07 11:24:33 +0530

    new fix in about us carousal

--- src/app/AboutUs/page.jsx
+++ src/app/AboutUs/page.jsx
@@ -5,7 +5,7 @@ import AboutUs_header from "@/Components/AboutUs_header/page"
 import AboutUs_threeCards from "@/Components/AboutUs_threeCards/page"
 import Marquee from "@/Components/Marquee/page"
 import AboutUs_Carousel2 from "@/Components/AboutUs_Carousel2/page"
-import AboutUs_Carousal from "@/Components/AboutUs_Carousal/page"
+// import AboutUs_Carousal from "@/Components/AboutUs_Carousal/page"
 import AboutUs_flex from "@/Components/AboutUs_flex/page"
 import AboutUsInfo from "@/Components/AboutUsInfo/page"
 import AboutUs_ourDesign from "@/Components/AboutUs_ourDesign/page"
@@ -26,7 +26,7 @@ const page = () => {
       <AboutUs_threeCards/>
       <Marquee/>
       <AboutUs_Carousel2/>
-      <AboutUs_Carousal/>
+      {/* <AboutUs_Carousal/> */}
       <AboutUs_flex/>
       <AboutUsInfo/>
       <AboutUs_ourDesign/>
